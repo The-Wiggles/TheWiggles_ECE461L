413d78da109de42aa913ce1a515e2108258bb5f6
Jason Zubia <zubiajason0@gmail.com>
2023-03-02 15:44:55 -0600

    Added two text boxes that say username and password

--- fruit-salad/src/App.js
+++ fruit-salad/src/App.js
@@ -6,15 +6,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <div>
+            <TextField required id="username" label="Username" variant="outlined" margin="normal"/>
+          </div>
+          <div>
+            <TextField required id="password" label="Password" variant="outlined" margin="dense"/>
+          </div>
+          <div>
+            <p>
+              Here is where we will place our login screen to start :) 
+            </p>
+          </div>
+        </div>
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <TextField id="username" label="Username" variant="outlined"/>
-        <TextField id="password" label="Password" variant="outlined"/>
-        
-        <p>
-          Here is where we will place our login screen to start :)
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
