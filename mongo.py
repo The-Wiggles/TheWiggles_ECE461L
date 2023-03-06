3e9c27274c6b8013c1825d283112a30d350fc85c
Zahid Hossain <zpangolin@outlook.com>
2023-03-05 22:20:00 -0600

    added .env

--- mongo.py
+++ mongo.py
@@ -1,6 +1,7 @@
 import pymongo
 import cipher
 
+# TODO: how to hide this connection string during deployment? is it even possible?
 db_connection_string = "mongodb+srv://admin:ECE461L_TheWiggles@cluster0.ypsn3um.mongodb.net/?retryWrites=true&w=majority"
 
 def addNewUser(userid, password):
@@ -13,7 +14,7 @@ def addNewUser(userid, password):
         return
 
     new_user_doc = {"userid": userid, "password": cipher.encrypt(password, 2, 1)}
-    users.insert_one(new_user_doc)
+    user_inserted_id = users.insert_one(new_user_doc).inserted_id
     client.close()
     return
 
