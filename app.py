51a367eec0ad4cbbab06beace43e57676afbc8fb
Zahid Hossain <zpangolin@outlook.com>
2023-03-06 10:21:05 -0600

    flask hello world

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
-from flask import Flask, redirect, url_for
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/admin')
-def hell_admin():
-    return 'Hello Admin'
-
-@app.route('/guest/<guest')
-def hello_guest(guest):
-    return 'Hello %s as Guest' % guest
-
-@app.route('/user/<name>')
-def hello_user(name):
-    if name == 'admin':
-        return redirect(url_for('hello_admin'))
-    else:
-        return redirect(url_for('hello_guest',guest=name))
+@app.route('/')
+def hello_world():
+    return 'Hello World'
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run()
