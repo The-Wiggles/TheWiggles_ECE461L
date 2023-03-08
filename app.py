ed9dc9a6b9828455fed053f70bb1d6dae72990c0
Zahid Hossain <zpangolin@outlook.com>
2023-03-07 21:06:41 -0600

    flask serve react app

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
+from flask.helpers import send_from_directory
+
+app = Flask(__name__, static_folder='./fruit-salad/build', static_url_path='/')
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/test')
+def test():
+    return 'test'
 
 if __name__ == '__main__':
     app.run()
